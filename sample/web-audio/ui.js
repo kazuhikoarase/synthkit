dbe537542733d6b0d18cf2129f6dfb1fef59bb97
Kazuhiko Arase <kazuhiko.arase@gmail.com>
2019-02-13 20:36:48 +0900

    commit

--- sample/web-audio/ui.js
+++ sample/web-audio/ui.js
@@ -1,7 +1,93 @@
 
 'use strict';
 
-Vue.component('WaveView', {
+Vue.component('SynVariable', {
+  template: '<span :style="bodyStyle">' +
+      '<span :style="knobStyle" @mousedown.prevent="mousedownHandler"></span>' +
+    '</span>',
+  props : {
+    type: { default: function() { return 'liner'; } },
+    value: { default: function() { return 0; } },
+    range: { default: function() { return [0, 100]; } },
+  },
+  methods : {
+    mousedownHandler: function(event) {
+      var mousemoveHandler = function(event) {
+        // calc value and emit.
+        this.left = Math.max(0, Math.min(
+            dragPoint.left + event.pageX - dragPoint.x,
+            this.width - this.height) );
+        var range = this.range;
+        var ratio = this.left / (this.width - this.height);
+        var value;
+        if (this.type == 'log') {
+          value = Math.exp( (Math.log(range[1]) - Math.log(range[0]) ) *
+              ratio + Math.log(range[0]) );
+        } else {
+          value = (range[1] - range[0]) * ratio + range[0];
+        }
+        value = +function(v){
+          if (v === 0) {
+            return 0;
+          }
+          var l = Math.log(v) / Math.log(10);
+          var p = Math.floor(4 - l);
+          return Math.floor(Math.exp(
+              (l + p) * Math.log(10) ) ) + 'E' + -p;
+        }(value);
+
+        this.$emit('input', value);
+
+      }.bind(this);
+      var mouseupHandler = function(event) {
+        document.removeEventListener('mousemove', mousemoveHandler);
+        document.removeEventListener('mouseup', mouseupHandler);
+      }.bind(this);
+      document.addEventListener('mousemove', mousemoveHandler);
+      document.addEventListener('mouseup', mouseupHandler);
+      var dragPoint = { x: event.pageX, y: event.pageY, left: this.left };
+    }
+  },
+  computed : {
+    bodyStyle : function() {
+      return {
+        display: 'inline-block', position: 'relative',
+        width: this.width + 'px',
+        height: this.height + 'px',
+        backgroundColor: 'rgba(0,0,0,0.2)'
+      };
+    },
+    knobStyle : function() {
+      var range = this.range;
+      var ratio;
+      if (this.type == 'log') {
+        ratio = (Math.log(this.value) - Math.log(range[0]) ) /
+            (Math.log(range[1]) - Math.log(range[0]) );
+      } else {
+        ratio = (this.value - range[0]) / (range[1] - range[0]);
+      }
+      ratio = Math.max(0, Math.min(ratio, 1) );
+      this.left = (this.width - this.height) * ratio;
+      return {
+        display: 'inline-block', position: 'absolute',
+        left: this.left + 'px',
+        top: '0px',
+        width: this.height + 'px',
+        height: this.height + 'px',
+        backgroundColor: 'rgba(0,0,0,0.6)'
+      };
+    }
+  },
+  data : function() {
+    return {
+      width: 200,
+      height: 16,
+      left: 0
+    };
+  }
+});
+
+Vue.component('SynWaveView', {
   template: '<canvas></canvas>',
   props : {
     wave: { },
